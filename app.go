3d79d14ee2d478df853078d201b14ce2ea6414aa
Yuwono Bangun Nagoro <morpheusgun@outlook.com>
2020-03-06 15:17:06 +0700

    add main func for main app(server) product, just saying hello to the world.

---
+++ app.go
@@ -0,0 +1,11 @@
+//Package main is the core package of kvothe-server
+package main
+
+import (
+	"fmt"
+)
+
+//func main is the main function of kvothe-server
+func main() {
+	fmt.Println("Hello world!")
+}
